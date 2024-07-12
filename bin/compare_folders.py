46f51b1328d5e1b7f65ffac6ac1602c6a4d9a3f9
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2024-07-12 12:41:30 +0200

    tested old vs new derivs

--- bin/compare_folders.py
+++ bin/compare_folders.py
@@ -13,6 +13,7 @@ import common_cfg.mpl_cfg as mpl_cfg
 plt.rcParams.update(mpl_cfg.mpl_rcParams_dict)
 start_time = time.perf_counter()
 
+
 def compare_param_cov_from_fm_pickles(fm_pickle_path_a, fm_pickle_path_b, compare_fms=True, compare_param_covs=True, plot=True):
 
     fm_dict_a = mm.load_pickle(fm_pickle_path_a)
@@ -33,23 +34,24 @@ def compare_param_cov_from_fm_pickles(fm_pickle_path_a, fm_pickle_path_b, compar
 
             cov_a = np.linalg.inv(fm_dict_a[key])
             cov_b = np.linalg.inv(fm_dict_b[key])
-            
+
             if compare_fms:
                 mm.compare_arrays(fm_dict_a[key], fm_dict_b[key], 'FM_A', 'FM_B', plot_diff_threshold=5)
 
             if compare_param_covs:
 
                 mm.compare_arrays(cov_a, cov_b, 'cov_A', 'cov_B', plot_diff_threshold=5)
-                
+
             if plot:
                 param_names = list(fm_dict_a['fiducial_values_dict'].keys())[:10]
                 fiducials_a = list(fm_dict_a['fiducial_values_dict'].values())[:10]
                 fiducials_b = list(fm_dict_b['fiducial_values_dict'].values())[:10]
-                uncert_a = mm.uncertainties_FM(fm_dict_a[key], 10, fiducials=fiducials_a, which_uncertainty='marginal', normalize=True)
-                uncert_b = mm.uncertainties_FM(fm_dict_b[key], 10, fiducials=fiducials_b, which_uncertainty='marginal', normalize=True)
+                uncert_a = mm.uncertainties_FM(
+                    fm_dict_a[key], 10, fiducials=fiducials_a, which_uncertainty='marginal', normalize=True)
+                uncert_b = mm.uncertainties_FM(
+                    fm_dict_b[key], 10, fiducials=fiducials_b, which_uncertainty='marginal', normalize=True)
                 diff = mm.percent_diff(uncert_a, uncert_b)
-                
-                
+
                 plt.figure()
                 plt.title(f'Marginalised uncertainties, {key}')
                 plt.plot(param_names, uncert_a, label='FM_A')
@@ -57,8 +59,9 @@ def compare_param_cov_from_fm_pickles(fm_pickle_path_a, fm_pickle_path_b, compar
                 plt.plot(param_names, diff, label='percent diff')
                 plt.legend()
 
-folder_a = '/home/davide/Documenti/Lavoro/Programmi/CLOE_validation/data/power_spectra/for_SPV3/v2.0.2/NL_flag04/v2_jul24/use_only_flat_modelsFalse/Omega_M'
-folder_b = '/home/davide/Documenti/Lavoro/Programmi/CLOE_validation/data/power_spectra/for_SPV3/v2.0.2/NL_flag04/v1_jan24/Omega_M'
+
+folder_a = f'/home/davide/Documenti/Lavoro/Programmi/CLOE_validation/data/power_spectra/for_SPV3/v2.0.2/NL_flag04/v2_jul24/use_only_flat_modelsTrue/{param_name}'
+folder_b = f'/home/davide/Documenti/Lavoro/Programmi/CLOE_validation/data/power_spectra/for_SPV3/v2.0.2/NL_flag04/v1_jan24/{param_name}'
 extension = 'dat'
 
 mm.test_folder_content(folder_a, folder_b, extension, verbose=True, rtol=1e-3)
@@ -67,14 +70,14 @@ for fm_name in os.listdir(folder_b):
     if fm_name.endswith('.txt'):
         fm_a = np.genfromtxt(f'{folder_a}/{fm_name}')
         fm_b = np.genfromtxt(f'{folder_b}/{fm_name}')
-        
+
         fm_a = mm.remove_null_rows_cols_2D_copilot(fm_a)
         fm_b = mm.remove_null_rows_cols_2D_copilot(fm_b)
-        
+
         cov_a = np.linalg.inv(fm_a)
         cov_b = np.linalg.inv(fm_b)
-        
+
         mm.compare_arrays(cov_a, cov_b, plot_diff_threshold=5)
-        
-        plt.figure()        
-        plt.plot((np.sqrt(np.diag(cov_a))/np.sqrt(np.diag(cov_b))-1)*100)
+
+        plt.figure()
+        plt.plot((np.sqrt(np.diag(cov_a)) / np.sqrt(np.diag(cov_b)) - 1) * 100)

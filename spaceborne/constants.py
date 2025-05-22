0ed36da63511ce25fb1799f3a7f312c861d62d95
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2025-05-22 11:41:11 +0200

    add a couple useful conversion factors

--- spaceborne/constants.py
+++ spaceborne/constants.py
@@ -3,3 +3,6 @@ import numpy as np
 DEG2_IN_SPHERE = 4 * np.pi * (180 / np.pi)**2
 DR1_DATE = 9191.0
 SPEED_OF_LIGHT = 299792.458  # km/s
+
+DEG2_TO_SR = (180 / np.pi) ** 2
+SR_TO_ARCMIN2 = (180 / np.pi * 60) ** 2

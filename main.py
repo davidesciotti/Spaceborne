5b7c891fc1c0d4784600e17db27fc5bd025ff60a
Davide Sciotti <84071067+davidesciotti@users.noreply.github.com>
2025-05-26 20:13:34 +0200

    don't save output as bench

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ cfg['OneCovariance']['path_to_oc_executable'] = '/home/davide/Documenti/Lavoro/P
 cfg['OneCovariance']['path_to_oc_ini'] = './input/config_3x2pt_pure_Cell_general.ini'
 cfg['OneCovariance']['consistency_checks'] = False
 
-cfg['misc']['save_output_as_benchmark'] = True
+cfg['misc']['save_output_as_benchmark'] = False
 cfg['misc']['bench_filename'] = (
     '../Spaceborne_bench/output_G{g_code:s}_SSC{ssc_code:s}_cNG{cng_code:s}'
     '_KE{use_KE:s}_resp{which_pk_responses:s}_b1g{which_b1g_in_resp:s}_devmerge3'

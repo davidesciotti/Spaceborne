b2be9a640e47d33eb66ff719124e98163ba7d8e5
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2025-05-29 15:37:11 +0200

    minor

--- spaceborne/julia_integrator.jl
+++ spaceborne/julia_integrator.jl
@@ -360,6 +360,7 @@ ind_cross = ind_cross .+ 1
 # this is for the 3x2pt covariance
 probe_combinations = (("L", "L"), ("G", "L"), ("G", "G"))
 
+println("\n*** Computing SSC ****")
 println("specs:")
 println("nbl: ", nbl)
 println("zbins: ", zbins)

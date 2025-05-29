3bb32bf2ae6fda6b96d785d18954f1f0af270a03
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2025-05-29 15:37:48 +0200

    add cov_sb_10d_to_heracles_dict func

--- spaceborne/sb_lib.py
+++ spaceborne/sb_lib.py
@@ -76,6 +76,108 @@ mpl_other_dict = {
 }
 
 
+def cov_sb_10d_to_heracles_dict(cov_10d, squeeze):
+    """
+    SB = 'Spaceborne'
+    HC = 'Heracles'
+
+
+    this dictionary specifies, within the 2 axes assigned to SHE, which ones
+    correspond to the E and B modes. This is not used since the analytical covariance
+    has no B modes.
+    This is also the reason why, regardless of probe and spin, the values are
+    stored in the 0-th index, i.e. arr_out[0, 0, 0, 0, :, :]
+    she_spin_dict = {
+        'E': 0,
+        'B': 1,
+    }
+    """
+
+    # this dictionary maps the SB probe indices to the HC probe names (keys)
+    probe_name_dict = {
+        0: 'POS',
+        1: 'SHE',
+    }
+
+    # this dictionary specifies the dimension of the corresponding axes in the output
+    # arrays. The dimensions correspond to the spin, except POS (spin-0) still needs 1
+    # dimension (not 0!)
+    probe_dims_dict = {
+        'POS': 1,
+        'SHE': 2,
+    }
+
+    # just a check
+    print('Translating covariance from Spaceborne to Heracles format...')
+
+    assert cov_10d.ndim == 10, 'input covariance is not 10-dimensional'
+    assert (
+        cov_10d.shape[0] == cov_10d.shape[1] == cov_10d.shape[2] == cov_10d.shape[3]
+    ), "The dimensions of the first 4 axes don't match"
+    assert cov_10d.shape[4] == cov_10d.shape[5], (
+        "The dimensions of the first 5th and 6th axes don't match"
+    )
+    assert (
+        cov_10d.shape[6] == cov_10d.shape[7] == cov_10d.shape[8] == cov_10d.shape[9]
+    ), "The dimensions of the last 4 axes don't match"
+
+    n_probes = cov_10d.shape[0]
+    zbins = cov_10d.shape[-1]
+    nbl = cov_10d.shape[4]
+
+    print(f'cov_10d shape = {cov_10d.shape}')
+    print(f'{n_probes = }')
+    print(f'{nbl = }')
+    print(f'{zbins = }')
+
+    cov_dict = {}
+
+    for probe_a_ix, probe_b_ix, probe_c_ix, probe_d_ix in itertools.product(
+        range(n_probes), repeat=4
+    ):
+        for zi, zj, zk, zl in itertools.product(range(zbins), repeat=4):
+            # get probe names and spins
+            probe_a_str = probe_name_dict[probe_a_ix]
+            probe_b_str = probe_name_dict[probe_b_ix]
+            probe_c_str = probe_name_dict[probe_c_ix]
+            probe_d_str = probe_name_dict[probe_d_ix]
+
+            probe_a_dims = probe_dims_dict[probe_a_str]
+            probe_b_dims = probe_dims_dict[probe_b_str]
+            probe_c_dims = probe_dims_dict[probe_c_str]
+            probe_d_dims = probe_dims_dict[probe_d_str]
+
+            arr_out = np.zeros(
+                shape=(
+                    probe_a_dims,
+                    probe_b_dims,
+                    probe_c_dims,
+                    probe_d_dims,
+                    nbl,
+                    nbl,
+                )
+            )
+
+            arr_out[0, 0, 0, 0, :, :] = cov_10d[  # fmt: skip
+                probe_a_ix, probe_b_ix, probe_c_ix, probe_d_ix,
+                :, :, zi, zj, zk, zl,
+            ]  # fmt: skip
+
+            if squeeze:
+                arr_out = np.squeeze(arr_out)
+
+            # fmt: off
+            cov_dict[
+                (probe_a_str, probe_b_str,
+                probe_c_str, probe_d_str,
+                zi, zj, zk, zl)
+            ] = arr_out
+
+    print('done')
+
+    return cov_dict
+
+
 # # Gaussian covariance binning
 # def bin_cov_gauss(cov, ell_values, theta_edges, fsky):
 #     binned_cov = np.zeros((len(theta_edges) - 1, len(theta_edges) - 1))

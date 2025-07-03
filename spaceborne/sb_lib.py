7b256536d181179f11464a271fb2990261f29c4e
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2025-07-03 17:50:01 +0200

    remove mpl pars dicts

--- spaceborne/sb_lib.py
+++ spaceborne/sb_lib.py
@@ -30,55 +30,6 @@ symmetrize_output_dict = {
 }
 
 
-mpl_rcParams_dict = {
-    'lines.linewidth': 1.5,
-    'font.size': 17,
-    'axes.labelsize': 'large',
-    'axes.titlesize': 'large',
-    'xtick.labelsize': 'large',
-    'ytick.labelsize': 'large',
-    #  'mathtext.fontset': 'stix',
-    #  'font.family': 'STIXGeneral',
-    'figure.figsize': (15, 10),
-    'lines.markersize': 8,
-    # 'axes.grid': True,
-    # 'figure.constrained_layout.use': False,
-    # 'axes.axisbelow': True
-}
-
-mpl_other_dict = {
-    'cosmo_labels_TeX': [
-        '$\\Omega_{{\\rm m},0}$',
-        '$\\Omega_{{\\rm b},0}$',
-        '$w_0$',
-        '$w_a$',
-        '$h$',
-        '$n_{\\rm s}$',
-        '$\\sigma_8$',
-        '${\\rm log}_{10}(T_{\\rm AGN}/{\\rm K})$',
-    ],
-    'IA_labels_TeX': ['$A_{\\rm IA}$', '$\\eta_{\\rm IA}$', '$\\beta_{\\rm IA}$'],
-    # 'galaxy_bias_labels_TeX': build_labels_TeX(zbins)[0],
-    # 'shear_bias_labels_TeX': build_labels_TeX(zbins)[1],
-    # 'zmean_shift_labels_TeX': build_labels_TeX(zbins)[2],
-    'cosmo_labels': ['Om', 'Ob', 'wz', 'wa', 'h', 'ns', 's8', 'logT'],
-    'IA_labels': ['AIA', 'etaIA', 'betaIA'],
-    # 'galaxy_bias_labels': build_labels(zbins)[0],
-    # 'shear_bias_labels': build_labels(zbins)[1],
-    # 'zmean_shift_labels': build_labels(zbins)[2],
-    'ylabel_perc_diff_wrt_mean': '$ \\bar{\\sigma}_\\alpha^i / \\bar{\\sigma}^{\\; m}_\\alpha -1 $ [%]',
-    'ylabel_sigma_relative_fid': '$ \\sigma_\\alpha/ \\theta^{fid}_\\alpha $ [%]',
-    'dpi': 500,
-    'pic_format': 'pdf',
-    'h_over_mpc_tex': '$h\\,{\\rm Mpc}^{-1}$',
-    'kmax_tex': '$k_{\\rm max}$',
-    'kmax_star_tex': '$k_{\\rm max}^\\star$',
-}
-
-
-
-
-
 # # Gaussian covariance binning
 # def bin_cov_gauss(cov, ell_values, theta_edges, fsky):
 #     binned_cov = np.zeros((len(theta_edges) - 1, len(theta_edges) - 1))
@@ -375,8 +326,6 @@ def j1(x):
 
 def j2(x):
     return jv(2, x)
-
-
 
 
 def savetxt_aligned(filename, array_2d, header_list, col_width=25, decimals=8):

7a405368028371cfd3a29fa588d4b43c656717da
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2025-07-14 15:21:26 +0200

    add hardcoded probe dictionaries

--- spaceborne/constants.py
+++ spaceborne/constants.py
@@ -3,3 +3,25 @@ import numpy as np
 DEG2_IN_SPHERE = 4 * np.pi * (180 / np.pi)**2
 DR1_DATE = 9191.0
 SPEED_OF_LIGHT = 299792.458  # km/s
+
+# admittedly, these are not constants ^^
+ALL_PROBE_COMBS = [
+    'LLLL', 'LLGL', 'LLGG',
+    'GLLL', 'GLGL', 'GLGG',
+    'GGLL', 'GGGL', 'GGGG',
+]  # fmt: skip
+
+DIAG_PROBE_COMBS = [
+    'LLLL',
+    'GLGL',
+    'GGGG',
+]
+
+PROBE_DICT = {'L': 0, 'G': 1}
+
+SYMMETRIZE_OUTPUT_DICT = {
+    ('L', 'L'): True,
+    ('G', 'L'): False,
+    ('L', 'G'): False,
+    ('G', 'G'): True,
+}

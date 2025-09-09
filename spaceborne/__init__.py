adfa6b04e5511ec630b9da8a57f3e056d8c6c8a5
davidesciotti <84071067+davidesciotti@users.noreply.github.com>
2025-09-09 10:21:21 +0100

    add cov_real_space

--- spaceborne/__init__.py
+++ spaceborne/__init__.py
@@ -5,6 +5,7 @@ from . import (
     config_checker,  # noqa: F401
     cosmo_lib,  # noqa: F401
     cov_harmonic_space,  # noqa: F401
+    cov_real_space,  # noqa: F401
     ell_utils,  # noqa: F401
     oc_interface,  # noqa: F401
     responses,  # noqa: F401

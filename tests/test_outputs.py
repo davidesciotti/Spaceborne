27326eea533676418fbcb550e70c1fe893888b54
Davide Sciotti <davidesciotti@gmail.com>
2025-09-15 18:17:41 +0200

    comment unused configs

--- tests/test_outputs.py
+++ tests/test_outputs.py
@@ -122,16 +122,16 @@ bench_yaml_names = [bench_name.replace('.npz', '') for bench_name in bench_yaml_
 bench_yaml_names.sort()
 
 # ... or run specific tests
-bench_yaml_names = [
-    'config_0006',
-    'config_0007',
-    'config_0008',
-    'config_0009',
-    'config_0010',
-    'config_0011',
-    'config_0012',
-    'config_0013',
-]
+# bench_yaml_names = [
+#     'config_0006',
+#     'config_0007',
+#     'config_0008',
+#     'config_0009',
+#     'config_0010',
+#     'config_0011',
+#     'config_0012',
+#     'config_0013',
+# ]
 
 main_script_path = f'{ROOT}/Spaceborne/main.py'
 temp_output_filename = f'{ROOT}/Spaceborne_bench/tmp/test_file'

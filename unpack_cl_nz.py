c1f6cd8e728c3e6a6dc7746c3f512d8e244ae468
Davide Sciotti <84071067+davidesciotti@users.noreply.github.com>
2026-02-05 12:45:13 +0100

    fix nz, it's just 1 z bin!

--- unpack_cl_nz.py
+++ unpack_cl_nz.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-nz = np.load(
-    '/Users/davidesciotti/Documents/Work/Code/common_data/Spaceborne_jobs/cov_validation_2026/input/nzs.npz'
+
+ROOT = (
+    '/Users/davidesciotti/Documents/Work/Code/DATA/Spaceborne_jobs/cov_validation_2026'
 )
+nz = np.load(f'{ROOT}/input/nzs.npz')
 
 # extract all arrays
 z = nz['z']
@@ -11,19 +13,32 @@ nz_1 = nz['nz_1']
 nz_2 = nz['nz_2']
 
 # convert to SB format
-nz_tab = np.column_stack((z, nz_1[0, :], nz_1[1, :]))
+nz_all = np.column_stack((z, nz_1[0, :], nz_1[1, :]))
 
 for zi in range(2):
-    plt.plot(nz_tab[:, 0], nz_tab[:, zi + 1], label=f'n(z) bin {zi + 1} (2D)')
+    plt.plot(nz_all[:, 0], nz_all[:, zi + 1], label=f'n(z) bin {zi + 1} (2D)')
 
-np.savetxt(
-    '/Users/davidesciotti/Documents/Work/Code/common_data/Spaceborne_jobs/cov_validation_2026/input/nzs.txt',
-    nz_tab,
-)
+nz_lns = np.column_stack((z, nz_1[0, :]))
+nz_src = np.column_stack((z, nz_1[1, :]))
+
+plt.figure()
+plt.plot(nz_src[:, 0], nz_src[:, 1], label='src')
+plt.plot(nz_lns[:, 0], nz_lns[:, 1], label='lens')
+plt.legend()
+plt.show()
+
+np.savetxt(f'{ROOT}/input/nz_src.txt', nz_src)
+np.savetxt(f'{ROOT}/input/nz_lns.txt', nz_lns)
+
+assert False, 'stop here'
+
 
 # now the Cls
 import euclidlib as el
-cl_dict = el.photo.angular_power_spectra('/Users/davidesciotti/Documents/Work/Code/common_data/Spaceborne_jobs/cov_validation_2026/input/cls_theory_lmax_3000.fits')
+
+cl_dict = el.photo.angular_power_spectra(
+    '/Users/davidesciotti/Documents/Work/Code/DATA/Spaceborne_jobs/cov_validation_2026/input/cls_theory_lmax_3000.fits'
+)
 
 nbl = cl_dict['PxP'].shape
 
@@ -32,5 +47,8 @@ cl_ll_3d = np.zeros()
 for zi in range(zbins):
     for zj in range(zbins):
         cl_ij = cl_dict['PxP'][zi, zj, 1, 1].data
-        plt.plot(cl_dict['PxP'][zi, zj, 1, 1].ell, cl_ij, label=f'C_ell bin {zi + 1} x bin {zj + 1}')
-
+        plt.plot(
+            cl_dict['PxP'][zi, zj, 1, 1].ell,
+            cl_ij,
+            label=f'C_ell bin {zi + 1} x bin {zj + 1}',
+        )
